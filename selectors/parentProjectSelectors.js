64c2ed15464b32950e97e3f2d323204d522f98aa
rewins <rhafiki.shantaram@gmail.com>
2024-07-10 00:38:06 -0700

    monofilament update test version

--- selectors/parentProjectSelectors.js
+++ selectors/parentProjectSelectors.js
@@ -4,9 +4,9 @@ const parentProjectSelectors = {
         dropdown: 'comp-lwzvacy9',
         optionText: 'Lady Grayy', 
         searchInput: 'input_comp-lx04dqgg1',
-        pageAssets: ['img-comp-lwzv3gwn2', 'img-comp-lwzv4kn911', 'img-comp-lwzv55il2', 'img-comp-lwzv4knh1', 'img-comp-lwzv4knn10'],
-        sections: ['comp-lwzv3gwi', 'comp-lwzv4kmz', 'comp-lwzv55h3'],
-        gURL_inputs: ['input_comp-lx2w05y5', 'input_comp-lx2w80l4', 'input_comp-lx2w8mxa'],
+        pageAssets: ['img-comp-lye58pqb1', 'img-comp-lydxb9ss', 'img-comp-lwzv3gwn2', 'img-comp-lwzv4kn911', 'img-comp-lwzv55il2', 'img-comp-lwzv4knh1', 'img-comp-lwzv4knn10'],
+        sections: ['comp-lydv3ffl', 'comp-lwzv3gwi', 'comp-lwzv4kmz', 'comp-lwzv55h3'],
+        gURL_inputs: ['input_comp-lydv543f', 'input_comp-lx2w05y5', 'input_comp-lx2w80l4', 'input_comp-lx2w8mxa'],
         googleDriveConfig: {
             "type": "service_account",
             "project_id": "descimgup",
