f17987d026f7b9f7b8c111827623cf325078fb00
rhafiki-shantaram <151375438+rhafiki-shantaram@users.noreply.github.com>
2024-09-13 00:20:04 -0700

    Update updateDescImg_helperFunctions.js - exChk edit

--- utils/updateDescImg_helperFunctions.js
+++ utils/updateDescImg_helperFunctions.js
@@ -3,10 +3,33 @@ const waitForImagesToLoad = async (page, imageSelectors) => {
     console.log('Waiting for images to load...');
     for (const selector of imageSelectors) {
         console.log(`Checking image ${selector}`);
+        
+        // Check if the image exists and is visible
+        const imageExists = await page.evaluate(selector => {
+            const img = document.querySelector(selector);
+            if (!img) return false;
+
+            const style = window.getComputedStyle(img);
+            const isVisible = style.display !== 'none' &&
+                style.visibility !== 'hidden' &&
+                style.opacity !== '0' &&
+                img.offsetHeight > 0 &&
+                img.offsetWidth > 0;
+
+            return isVisible;
+        }, selector);
+
+        if (!imageExists) {
+            console.log(`Image ${selector} does not exist or is not visible, skipping...`);
+            continue; // Skip to the next image selector if the image doesn't exist or is not visible
+        }
+
+        // Check if the image is fully loaded
         const imageLoaded = await page.evaluate(selector => {
             const img = document.querySelector(selector);
             return img && img.complete && img.naturalHeight !== 0;
         }, selector);
+
         if (!imageLoaded) {
             console.log(`Image ${selector} not loaded yet, waiting...`);
             await page.waitForFunction(selector => {
@@ -20,6 +43,7 @@ const waitForImagesToLoad = async (page, imageSelectors) => {
     }
     console.log('All images loaded.');
 };
+
 
 const getElementDimensions = async (page, selector, scaleFactor) => {
     const dimensions = await page.evaluate(selector => {
